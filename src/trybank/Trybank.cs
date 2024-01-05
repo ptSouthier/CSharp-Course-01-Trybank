9f3995ab04fe8516421b78dd813dc8e4b9e84342
ptSouthier <patrick.southier@hotmail.com>
2024-01-05 17:06:46 -0300

    feat: implementing "Logout" function according the req 3 specs

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -80,7 +80,15 @@ public class TrybankLib
     // 3. Construa a funcionalidade de fazer Logout
     public void Logout()
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado");
+        }
+        else
+        {
+            Logged = false;
+            loggedUser = -99;
+        }
     }
 
     // 4. Construa a funcionalidade de checar o saldo
